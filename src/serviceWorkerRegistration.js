03e5c3187dcacb76e6933388bda92e7033710f4f
elo-world <175364541+elo-world@users.noreply.github.com>
2026-04-23 10:09:37 +0200

    Fix Service Worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -13,7 +13,7 @@ export function register(config) {
         if (publicUrl.origin !== window.location.origin) return;
 
         window.addEventListener("load", () => {
-            const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
+            const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
             if (isLocalhost) {
                 checkValidServiceWorker(swUrl, config);
