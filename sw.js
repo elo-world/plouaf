1e134f71b0c576806bc647b16fd558369c4f758f
elo-world <175364541+elo-world@users.noreply.github.com>
2026-04-26 21:58:44 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-env serviceworker */
 
-const CACHE_VERSION = "v1.0.2";
+const CACHE_VERSION = "v1.0.3";
 var urlsToCache = [
     "./",
     "/plouaf/images/duck/blue.svg",
