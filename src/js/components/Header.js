f825494befbac5b18eb9e52cad19d4c81696443f
elo-world <175364541+elo-world@users.noreply.github.com>
2026-05-01 23:42:43 +0200

    Fix: Error jsx.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -50,10 +50,10 @@ function Header({ show, status, toggleMenu, toggleLanguagesMenu }) {
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="lucide lucide-menu-icon lucide-menu"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        stroke-Linejoin="round"
+                        className="lucide lucide-menu-icon lucide-menu"
                     >
                         <path d="M4 5h16" />
                         <path d="M4 12h16" />
